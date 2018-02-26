58812c0264ee8e4c8373a2ae3b66a268c21cbe2b
wendingp <wendingp@gmail.com>
2018-02-26 19:58:13 +0800

    Adds pa1 skeleton

---
+++ pa1/mall.cpp
@@ -0,0 +1,37 @@
+//Submit this file
+#include "mall.h"
+//You are NOT allowed to include any additional library
+
+//NOTE: Mall::duplicateShopLinkedList must NOT be implemented here
+
+Mall::Mall(string name, string address) {
+
+}
+
+Mall::Mall(Mall &another) {
+
+}
+
+Mall::~Mall() {
+
+}
+
+void Mall::setName(string name) {
+
+}
+
+void Mall::setAddress(string address) {
+
+}
+
+string Mall::getName() {
+
+}
+
+string Mall::getAddress() {
+
+}
+
+bool Mall::addShop(string name, int shopNumber) {
+
+}
