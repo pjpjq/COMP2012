ae7e509bffbcd9ccbc3c0e1d5577c9c3bbd47c55
wendingp <wendingp@gmail.com>
2018-02-26 11:19:33 +0800

    Finishes skeleton.

--- lab2/file.cpp
+++ lab2/file.cpp
@@ -5,44 +5,34 @@
 
 using namespace std;
 
-File::File()
-{
-	name = nullptr;
+File::File() {
+	name = '\0';
 }
 
-File::~File()
-{
-	if (name != nullptr) {
+File::~File() {
+	if (name) {
 		delete name;
 	}
 }
 
-File::File(const File &file)
-{
-	if (file.name != nullptr) {
-		name = new char[strlen(file.name) + 1];
-		strcpy(name, file.name);
-	}
+File::File(const File &file) {
+	name = new char[strlen(file.name) + 1];
+	strcpy(name, file.name);
 }
 
-File::File(const char *fileName)
-{
+File::File(const char *fileName) {
 	name = new char[strlen(fileName) + 1];
 	strcpy(name, fileName);
 }
 
-bool File::equals(const File &file) const
-{
-	if (name == nullptr && file.name == nullptr) return true;
-	if (name == nullptr || file.name == nullptr) return false;
-
+bool File::equals(const File &file) const {
 	return strcmp(name, file.name) == 0;
 }
 
-void File::print() const
-{
-	if (name != nullptr)
+void File::print() const {
+	if (name) {
 		cout << "File " << name << endl;
-	else
+	} else {
 		cout << "File without name" << endl;
+	}
 }
--- lab2/folder.cpp
+++ lab2/folder.cpp
@@ -4,32 +4,48 @@
 
 using namespace std;
 
-Folder::Folder(unsigned int size)
-{
-	
+Folder::Folder(unsigned int size) {
+	files = new const File*[size];
+	maxSize = size;
+	currSize = 0;
 }
 
-Folder::~Folder()
-{
-	
+Folder::~Folder() {
+	if (files) {
+		delete [] files;
+	}
 }
 
-Folder::Folder(const Folder &folder)
-{
-	
+Folder::Folder(const Folder &folder) {
+	maxSize = folder.maxSize;
+	currSize = folder.currSize;
+	files = new const File*[maxSize];
+	for (int i = 0; i < currSize; ++i) {
+		files[i] = new const File(*(folder.files[i]));
+	}
 }
 
-void Folder::addFile(const File &file)
-{
-	
+void Folder::addFile(const File &file) {
+	if (contains(file)) {
+		cout << "File already exists in folder" << endl;
+		return;
+	}
+	if (currSize == maxSize) {
+		cout << "Insufficient space for folder" << endl;
+		return;
+	}
+	files[currSize++] = &file;
 }
 
-bool Folder::contains(const File &file) const
-{
-	return false; // Dummy statement. You need to update it with your own code.
+bool Folder::contains(const File &file) const {
+	for (auto &file_ptr : files) {
+		if (file.equals(*file_ptr)) {
+			return true;
+		}
+	}
+	return false;
 }
 
-void Folder::print() const
-{
+void Folder::print() const {
 	
 }
